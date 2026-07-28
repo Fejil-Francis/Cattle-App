fdadf576c6ae7100cfa4708d5e81835a9effcf8d
unknown <fejilfrancis@gmail.com>
2026-07-28 18:32:44 +0530

    Initial commit

---
+++ SQL/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE smart_dairy;
+
+\c smart_dairy;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
